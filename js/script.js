fef17b58d87a708323f6abeb4dc499af696aaa44
Alex100688 <alessiozingaro2@gmail.com>
2022-10-26 11:01:42 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const parola= prompt("Inserisci la parola");
+const parolaDue=prompt ("Inserisci la parola");
+
+let frase= false;
+
+if(parola>parolaDue){
+    frase=true;
+    console.log(frase)
+   
+}
+else if (parola<parolaDue)
+    {
+        frase=false;
+    }
