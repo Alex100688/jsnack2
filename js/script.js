6f881753062303838aabebea0cf4dde5d26d5a36
Alex100688 <alessiozingaro2@gmail.com>
2022-10-26 11:22:28 +0200

    ....

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const parola= prompt("Inserisci la parola");
 const parolaDue=prompt ("Inserisci la parola");
 
-let frase= true;
 
-if(parola>parolaDue){
-    frase=true;
-    console.log(frase)
-   
+
+if (parola > parolaDue ) {
+    alert("bravo");
+    
 }
-else if (parola<parolaDue)
-    {
-        frase=false;
-        console.log(frase)
-    }
+else {
+    alert("Non bravo")
+}
+
+    
